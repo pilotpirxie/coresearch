7771a9511658cdfbe31b0e3942d42ed3d9484f78
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2019-11-09 02:12:29 +0100

    Add Insert and Get methods
    Make demo app

---
+++ coresearch/Coresearch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace coresearch
+{
+    class Coresearch
+    {
+        private SortedDictionary<string, HashSet<string>> words = new SortedDictionary<string, HashSet<string>>();
+        private int _count = 0;
+
+        public int Count {
+            get => _count;
+        }
+
+        public void Insert(string name, string content, string meta)
+        {
+            string[] contentWords = content.Split(' ');
+
+            foreach (string word in contentWords)
+            {
+                Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+                string _word = rgx.Replace(word, "");
+                _count++;
+
+                if (Count % 50000 == 0)
+                {
+                    Console.WriteLine($"Batch {Count}");
+                }
+
+                if (words.ContainsKey(_word))
+                {
+                    words[_word].Add(name);
+                }
+                else
+                {
+                    words.Add(_word, new HashSet<string>() { name });
+                }
+            }
+        }
+
+        public List<string> Get(string word)
+        {
+            Regex rgx = new Regex("[^a-zA-Z0-9 -]");
+            string _word = rgx.Replace(word, "");
+            List<string> toReturn = new List<string>();
+
+            if (words.ContainsKey(_word))
+            {
+                HashSet<string> hs = words[_word];
+                foreach (string el in hs)
+                {
+                    toReturn.Add(el);
+                }
+            }
+
+            return toReturn;
+        }
+    }
+}
---
+++ coresearch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace coresearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coresearch coresearch = new Coresearch();
+
+            int i = 0;
+            foreach (string file in Directory.EnumerateFiles("./bbc", "*.txt", SearchOption.AllDirectories))
+            {
+                string contents = File.ReadAllText(file);
+                coresearch.Insert(file, contents, file);
+                i++;
+            }
+
+            Console.WriteLine($"Files added {i}");
+            Console.WriteLine($"Words inserted ${coresearch.Count}");
+
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+
+                List<string> results = coresearch.Get(userInput);
+
+                foreach (string el in results)
+                {
+                    Console.WriteLine(el);
+                }
+            }
+        }
+    }
+}
