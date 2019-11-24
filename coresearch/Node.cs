98b1fadbc584c10b3950af042962e1677c1b5929
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2019-11-24 22:52:17 +0100

    Add operations on coresearch structure from cli

---
+++ coresearch/Node.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace coresearch
+{
+    public class Node
+    {
+        private Node _parent;
+        private char _key;
+        private HashSet<string> _data = new HashSet<string>();
+        private List<Node> _children = new List<Node>();
+        private int _depth = 0;
+
+        internal Node Parent { get => _parent; set => _parent = value; }
+        public char Key { get => _key; set => _key = value; }
+        public HashSet<string> Data { get => _data; set => _data = value; }
+        public List<Node> Children { get => _children; set => _children = value; }
+        public int Depth { get => _depth; set => _depth = value; }
+
+        public Node(Node parent, char key, string data = null, int depth = 0)
+        {
+            Parent = parent;
+            Key = key;
+            if (data != null)
+            {
+                Data.Add(data);
+            }
+            Depth = depth;
+        }
+
+        public Node GetChildByKey(char key)
+        {
+            for (int i = 0; i < _children.Count; i++)
+            {
+                if (_children[i].Key == key)
+                {
+                    return _children[i];
+                }
+            }
+
+            return null;
+        }
+
+        public void DeleteChildByKey(char key)
+        {
+            for (int i = 0; i < _children.Count; i++)
+            {
+                if (_children[i].Key == key)
+                {
+                    _children.RemoveAt(i);
+                }
+            }
+        }
+
+        public bool IsLeaf()
+        {
+            return _children.Count == 0 && _parent != null;
+        }
+
+        public bool ContainsData()
+        {
+            return _data != null;
+        }
+
+        public bool ContainsData(string dataToCompare)
+        {
+            return _data.Contains(dataToCompare);
+        }
+    }
+}
--- coresearch/Program.cs
+++ coresearch/Program.cs
@@ -4,35 +4,93 @@ using System.IO;
 
 namespace coresearch
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        private static int _filesCount = 0;
+        private static Coresearch _coresearch;
+        private static string prompt = "> ";
+        private static void LoadFromSource(string path, string extension)
         {
-            // sample data http://mlg.ucd.ie/datasets/bbc.html
-            Coresearch coresearch = new Coresearch(true);
-            int filesCount = 0;
-
-            foreach (string file in Directory.EnumerateFiles("./", "*.txt", SearchOption.AllDirectories))
+            foreach (string file in Directory.EnumerateFiles(path, extension, SearchOption.AllDirectories))
             {
-                filesCount++;
+                _filesCount++;
                 foreach (string line in File.ReadLines(file))
                 {
-                    coresearch.InsertResource(file, line.Replace(";", ""));
+                    _coresearch.InsertResource(file, line);
                 }
             }
 
-            Console.WriteLine($"Words inserted {coresearch.Count} from {filesCount} files with memory usage of {GC.GetTotalMemory(false)}");
+            Console.WriteLine($"Words inserted {_coresearch.Count} from {_filesCount} files with memory usage of {GC.GetTotalMemory(false)}");
+        }
+
+        private static void Search(string key)
+        {
+            List<string> results = _coresearch.Get(key);
+            Console.WriteLine($"{results.Count} results for {key}");
+
+            foreach (string el in results)
+            {
+                Console.WriteLine(el);
+            }
+        }
+
+        private static void Insert(string resourceName, string content)
+        {
+            _coresearch.InsertResource(resourceName, content);
+        }
+
+        private static void Delete(string key)
+        {
+            _coresearch.Remove(key);
+        }
+
+        private static void Echo(string content)
+        {
+            Console.WriteLine(_coresearch.Echo(content));
+        }
+
+        private static void Flush()
+        {
+                _coresearch.Flush();
+        }
+
+        static void Main(string[] args)
+        {
+            // sample data http://mlg.ucd.ie/datasets/bbc.html
+            _coresearch = new Coresearch(true);
 
             while (true)
             {
+                Console.Write(prompt);
                 string userInput = Console.ReadLine();
 
-                List<string> results = coresearch.Get(userInput);
-                Console.WriteLine($"{results.Count} results for {userInput}");
+                string[] command = userInput.Split(' ');
 
-                foreach (string el in results)
+                switch (command[0])
                 {
-                    Console.WriteLine(el);
+                    case "source":
+                    case "load":
+                        if (command.Length == 3) LoadFromSource(command[1], command[2]);
+                        break;
+                    case "get":
+                    case "search":
+                        if (command.Length == 2) Search(command[1]);
+                        break;
+                    case "add":
+                    case "insert":
+                        if (command.Length == 3) Insert(command[1], command[2]);
+                        break;
+                    case "delete":
+                        if (command.Length == 2) Delete(command[1]);
+                        break;
+                    case "echo":
+                        if (command.Length == 2) Echo(command[1]);
+                        break;
+                    case "flush":
+                        Flush();
+                        break;
+                    default:
+                        break;
                 }
             }
         }
